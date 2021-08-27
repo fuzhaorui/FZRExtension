abd1d39a7693ad3e129c7ffbdf8838de8b72c4de
FZR <595878709@qq.com>
2021-08-27 17:08:00 +0800

    更新1.0.2

--- FZRExtension.podspec
+++ FZRExtension.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'FZRExtension'
-  s.version          = '1.0.1'
+  s.version          = '1.0.2'
   s.summary          = '私人扩展'
 
 # This description is used to generate tags and improve search results.
@@ -36,7 +36,7 @@ TODO: Add long description of the pod here.
   #   'FZRExtension' => ['FZRExtension/Assets/*.png']
   # }
 
-  s.public_header_files = 'FZRExtension/Classes/**/*.h'
-  # s.frameworks = 'UIKit', 'MapKit'
+  # s.public_header_files = 'FZRExtension/Classes/**/*.h'
+  s.frameworks = 'UIKit', 'Foundation'
   # s.dependency 'AFNetworking', '~> 2.3'
 end
--- FZRExtension/Classes/Extension/UIWindow/UIWindow+Extension.m
+++ FZRExtension/Classes/Extension/UIWindow/UIWindow+Extension.m
@@ -12,27 +12,27 @@
 
 /// MARK: - 系统级UIWindow
  +(UIWindow *)keyWindow{
+     
     UIWindow *keyWindow = nil;
-#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 130000 // 编译时检查SDK版本：Xcode11+编译会调用（iOS SDK 13.0 以后版本的处理）
+
     if (@available(iOS 13.0, *)) {
-        NSSet<UIScene *> *connectedScenes = [UIApplication sharedApplication].connectedScenes;
-        for (UIScene *scene in connectedScenes) {
-            if (scene.activationState == UISceneActivationStateForegroundActive && [scene isKindOfClass:[UIWindowScene class]]) {
-                UIWindowScene *windowScene = (UIWindowScene *)scene;
-                for (UIWindow *window in windowScene.windows) {
-                    if (window.isKeyWindow) {
-                        keyWindow = window;
-                        break;
-                    }
-                }
+        
+        for (UIWindowScene *windowScene in [UIApplication sharedApplication].connectedScenes) {
+            
+            if (windowScene.activationState == UISceneActivationStateForegroundActive) {
+                
+                keyWindow = windowScene.windows.firstObject;
+                
+                break;
+                
             }
+            
         }
-    } else
-#endif
-    {
-#if __IPHONE_OS_VERSION_MIN_REQUIRED < 130000
-        return  [UIApplication sharedApplication].keyWindow;
-#endif
+        
+    } else{
+
+        keyWindow = [UIApplication sharedApplication].keyWindow;
+
     }
     
     return keyWindow;
